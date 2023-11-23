20f83993f90dcb5503cd0d7e5b5e31b0575244ae
David Nanculeo <dnanculeob@uft.edu>
2023-11-22 21:55:07 -0300

    Elimina importaciones innecesarias

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 //importaciones de componentes
 import Navbar from './components/navbar/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
     <Navbar></Navbar>
