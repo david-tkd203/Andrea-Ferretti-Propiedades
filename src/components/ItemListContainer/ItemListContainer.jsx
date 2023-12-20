f47f5e92867763f4402b52c77eb170a33b665e0f
David Nanculeo <dnanculeob@uft.edu>
2023-12-20 20:41:59 -0300

    agregado de descripcion

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -4,12 +4,12 @@ import './ItemListContainer.css';
 
 function ItemListContainer() {
   const items = [
-    { id: 1, name: "Propiedad en arriendo", category: "Casa", image: `./propiedades arriendo/CASA_ARRIENDO_${id}.jpeg`, comuna:  "Curico", operacion: "Venta"},
-    { id: 2, name: "Propiedad en venta", category: "Casa", image: `./propiedades en venta/CASA_VENTA_${id}.jpeg`, comuna:  "Curico", operacion: "Arriendo"},
+    { id: 1, name: "Propiedad en arriendo", category: "Casa", image: "./propiedades arriendo/CASA_ARRIENDO_1.jpeg", descripcion: "Departamento bastante cómodo e iluminado y de tamaño acorde a su capacidad emplazado en QUINTO piso, tiene orientación surponiente. El condominio cuenta con una excelente conectividad y en su equipamiento destaca la proximidad a cuatro excelentes colegios, además a supermercado, centro comercial, panaderías, almacenes, la locomoción pública transita por fuera del condominio.", comuna: "Curico", operacion: "Venta" },
+    { id: 2, name: "Propiedad en venta", category: "Casa", image: "./propiedades en venta/CASA_VENTA_1.jpeg", descripcion: "Casa que se encuentra emplazada en calle, a una cuadra de la Plaza San Francisco, Avenida España, próximo a supermercados, comercio y a menos de 10’ caminando del centro cívico de la ciudad. Construcción aislada, de albañilería y en un nivel. El piso de la mayoría de los espacios de la propiedad es de parquet.", comuna: "Curico", operacion: "Arriendo" },
   ];
 
-  const arriendos = items.filter(item => item.operacion === "arriendo");
-  const ventas = items.filter(item => item.operacion === "venta");
+  const arriendos = items.filter(item => item.operacion === "Venta");
+  const ventas = items.filter(item => item.operacion === "Arriendo");
 
   return (
     <div className="item-list-container">
@@ -21,6 +21,7 @@ function ItemListContainer() {
               <img src={item.image} alt={item.name} />
               <div>
                 <h3>{item.name}</h3>
+                <p>{item.descripcion}</p>
                 <span> - {item.category}</span>
               </div>
             </Link>
@@ -30,16 +31,17 @@ function ItemListContainer() {
       <h1>Arriendos</h1>
       <ul>
         {arriendos.map((item) => (
-            <li key={item.id}>
-              <Link to={`/item/${item.id}`}>
-                <img src={item.image} alt={item.name} />
-                <div>
-                  <h3>{item.name}</h3>
-                  <span> - {item.category}</span>
-                </div>
-              </Link>
-            </li>
-          ))}
+          <li key={item.id}>
+            <Link to={`/item/${item.id}`}>
+              <img src={item.image} alt={item.name} />
+              <div>
+                <h3>{item.name}</h3>
+                <p>{item.descripcion}</p>
+                <span> - {item.category}</span>
+              </div>
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
