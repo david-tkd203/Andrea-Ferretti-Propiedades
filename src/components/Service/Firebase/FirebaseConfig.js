a74d88b05f4054185a6ebb7876be8841c5000389
David Ñ <dnanculeob@uft.edu>
2024-01-14 22:35:04 -0300

    Agregado de firebase

---
+++ src/components/Service/Firebase/FirebaseConfig.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBsXl_v_QyeyzlML4-wp55ecof3L1espF4",
+  authDomain: "andreafp-b2d55.firebaseapp.com",
+  projectId: "andreafp-b2d55",
+  storageBucket: "andreafp-b2d55.appspot.com",
+  messagingSenderId: "1060838457210",
+  appId: "1:1060838457210:web:eae41718146f6533aca0c3",
+  measurementId: "G-RL6BGFWZXG"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
