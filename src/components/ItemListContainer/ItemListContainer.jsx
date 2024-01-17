e2b2fe810699cac3d2882cee49bd4ac1796abda3
David Ñ <dnanculeob@uft.edu>
2024-01-16 23:39:58 -0300

    arreglo de componentes

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import './ItemListContainer.css';
 import { getDocs, collection } from "firebase/firestore";
 import { db } from "../Service/Firebase/FirebaseConfig";
+import { ItemListSection } from '../ItemListSection/ItemListSection'
+import { FilterSection } from '../FilterSection/FilterSection'
 
 const ItemListContainer = ({ greeting }) => {
   const [loading, setLoading] = useState(true);
@@ -13,7 +15,7 @@ const ItemListContainer = ({ greeting }) => {
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
-      const collectionRef = collection(db, 'products');
+      const collectionRef = collection(db, 'ventas');
       
       try {
         const querySnapshot = await getDocs(collectionRef);
@@ -45,48 +47,10 @@ const ItemListContainer = ({ greeting }) => {
   return (
     <div className="item-list-container">
       <FilterSection filtro={filtro} onFilterChange={handleFilterChange} />
-      <ItemListSection title="Propiedades" items={filteredProductos} />
+      <ItemListSection title="Propiedades" {...filteredProductos} />
     </div>
   );
 }
 
-const FilterSection = ({ filtro, onFilterChange }) => (
-  <div>
-    <h2>Filtrar por:</h2>
-    <select value={filtro} onChange={onFilterChange}>
-      <option value="todos">Todos</option>
-      <option value="Arriendo">Arriendos</option>
-      <option value="Venta">Ventas</option>
-    </select>
-  </div>
-);
-
-const ItemListSection = ({ title, items }) => (
-  <>
-    <h1>{title}</h1>
-    <ul>
-      {items.map((item) => (
-        <li key={item.id}>
-          <div>
-            <img src={item.image} alt={item.name} />
-            <div>
-              <h3>
-                <Link to={`/items/${item.id}`}>{item.name}</Link>
-              </h3>
-              <span> - {item.category}</span>
-              <p>{item.descripcion}</p>
-              <p>Comuna: {item.comuna}</p>
-              <p>Baño: {item.baño}</p>
-              <p>Habitaciones: {item.habitaciones}</p>
-              <p>Estacionamientos: {item.estacionamientos}</p>
-              <p>Precio: {item.precio}</p>
-              <p>Tipo: {item.tipo}</p>
-            </div>
-          </div>
-        </li>
-      ))}
-    </ul>
-  </>
-);
 
 export default ItemListContainer;
